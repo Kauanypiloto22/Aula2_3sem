fb11128fba5f1f64f17dc6ee3f30d8b6af116205
unknown <kauany.piloto@yahoo.com>
2026-02-03 11:32:05 -0300

    Atividade 2

---
+++ ControlescolarApplication.java
@@ -0,0 +1,13 @@
+package com.controlescolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlescolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlescolarApplication.class, args);
+	}
+
+}
---
+++ TurmaEntity.java
@@ -0,0 +1,5 @@
+package com.controlescolar;
+
+public class TurmaEntity {
+
+}
